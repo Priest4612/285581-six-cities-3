2b9b3679e4b9d488bec63b21dd5fc42657ef5e5a
priest4612 <prie4612@gmail.com>
2020-02-06 18:19:47 +0300

    Установил React. Создал компоненты

---
+++ src/components/app/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Main from '../main.jsx';
+
+const App = () => {
+  // eslint-disable-next-line react/prop-types
+  return (
+    <Main />
+  );
+};
+
+export default App;
