1ecd46dee18f2dc63e1642446efa5a2c023ecc65
Priest4612 <prie4612@gmail.com>
2020-02-17 13:32:24 +0300

    Добавил моки

---
+++ src/mock/offers.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    id: 101,
+    isPremium: true,
+    picture: `apartment-01.jpg`,
+    price: 120,
+    isBookmark: false,
+    rating: 4,
+    name: `Beautiful & luxurious apartment at great location`,
+    type: `Apartment`,
+  }, {
+
+    id: 202,
+    isPremium: false,
+    picture: `room.jpg`,
+    price: 80,
+    isBookmark: true,
+    rating: 4,
+    name: `Wood and stone place`,
+    type: `Private room`,
+  }, {
+
+    id: 333,
+    isPremium: true,
+    picture: `apartment-03.jpg`,
+    price: 180,
+    isBookmark: true,
+    rating: 5,
+    name: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`,
+  }, {
+
+    id: 400,
+    isPremium: false,
+    picture: `apartment-02.jpg`,
+    price: 132,
+    isBookmark: true,
+    rating: 4,
+    name: `Canal View Prinsengracht`,
+    type: `Apartment`,
+  }
+];
--- src/mock/rental-offers.js
+++
@@ -1,5 +0,0 @@
-const offers = [
-
-];
-
-export {offers};
