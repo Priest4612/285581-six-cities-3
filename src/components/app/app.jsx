38a1684082f1cbdc587c48c2322b830c6d159f65
priest4612 <prie4612@gmail.com>
2020-02-21 00:53:16 +0300

    Правки

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
 
 import Main from '../main/main.jsx';
-import OfferDetails from '../../offer-details/offer-details.jsx';
+import OfferDetails from '../offer-details/offer-details.jsx';
 
 const ViewMode = {
   MAIN: `main`,
