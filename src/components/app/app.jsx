6b77ba14c3315c6c84b7439979812e65fd7b684f
priest4612 <prie4612@gmail.com>
2020-02-21 00:49:55 +0300

    Добавил код перехода на страницу предложения. Обновил тест

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -5,10 +5,22 @@ import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import Main from '../main/main.jsx';
 import OfferDetails from '../../offer-details/offer-details.jsx';
 
+const ViewMode = {
+  MAIN: `main`,
+  DETAIL: `detail`,
+};
+
 
 class App extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.state = {
+      viewMode: ViewMode.MAIN,
+      activeOffer: null,
+    };
+
+    this._handleRentalHeaderClick = this._handleRentalHeaderClick.bind(this);
   }
 
   render() {
@@ -31,17 +43,29 @@ class App extends PureComponent {
   }
 
   _renderApp(offers) {
-
-    return (
-      <Main
-        offers={offers}
-        handleRentalHeaderClick={this._handleRentalHeaderClick}
-      />
-    );
+    switch (this.state.viewMode) {
+      case ViewMode.MAIN:
+        return (
+          <Main
+            offers={offers}
+            handleRentalHeaderClick={this._handleRentalHeaderClick}
+          />
+        );
+      case ViewMode.DETAIL:
+        return (
+          <OfferDetails
+            offer={this.state.activeOffer[0]}
+          />
+        );
+    }
+    return null;
   }
 
-  _handleRentalHeaderClick(/* id */) {
-    return/*  id */;
+  _handleRentalHeaderClick(id) {
+    this.setState({
+      viewMode: ViewMode.DETAIL,
+      activeOffer: this.props.offers.filter((offer) => offer.id === id),
+    });
   }
 }
 
--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+
 import App from '../app/app.jsx';
 
 const offers = [
@@ -12,16 +13,71 @@ const offers = [
     rating: 4,
     name: `apartment-01`,
     type: `Apartment`,
+
+    gallery: [
+      `apartment-11.jpg`,
+      `apartment-02.jpg`,
+      `apartment-33.jpg`,
+      `room.jpg`,
+      `studio-01.jpg`,
+      `apartment-01.jpg`
+    ],
+    bedroomsAmount: 11,
+    maxAdults: 10,
+    features: [
+      `Wi-Fi`,
+      `Washing machine`,
+      `Towels`,
+      `Heating`,
+      `Coffee machine`,
+      `Baby seat`,
+      `Kitchen`,
+      `Dishwasher`,
+      `Fridge`
+    ],
+    hostName: `Angelina`,
+    hostAvatar: `img/avatar-angelina.jpg`,
+    hostStatus: `pro`,
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.
+                  The 18th century.
+                  An independent House, strategically located between Rembrand Square and National Opera,
+                  but where rest in this alley flowery and colorful.`
   }, {
 
     id: 202,
     isPremium: false,
     picture: `room.jpg`,
-    price: 88,
+    price: 828,
     isBookmark: true,
     rating: 3,
-    name: `room`,
+    name: `Wood and stone place 222`,
     type: `Private room`,
+
+    gallery: [
+      `apartment-01.jpg`,
+      `apartment-02.jpg`,
+      `apartment-03.jpg`,
+      `room.jpg`,
+      `studio-01.jpg`,
+      `apartment-01.jpg`
+    ],
+    bedroomsAmount: 3,
+    maxAdults: 9,
+    features: [
+      `Wi-Fi`,
+      `Heating`,
+      `Coffee machine`,
+      `Baby seat`,
+      `Kitchen`,
+      `Dishwasher`,
+      `Cabel TV`,
+      `Fridge`
+    ],
+    hostName: `Max`,
+    hostAvatar: `img/avatar-max.jpg`,
+    hostStatus: `pro`,
+    description: `An independent House, strategically located between Rembrand Square and National Opera,
+                  but where the bustle of the city comes to rest in this alley flowery and colorful.`
   }, {
 
     id: 333,
@@ -30,18 +86,70 @@ const offers = [
     price: 218,
     isBookmark: false,
     rating: 4.5,
-    name: `apartment-03`,
+    name: `Nice, cozy, warm big bed apartment`,
     type: `Apartment`,
-  }, {
 
+    gallery: [
+      `apartment-02.jpg`,
+      `apartment-01.jpg`,
+      `room.jpg`,
+      `apartment-03.jpg`,
+      `apartment-01.jpg`,
+      `studio-01.jpg`,
+    ],
+    bedroomsAmount: 2,
+    maxAdults: 2,
+    features: [
+      `Wi-Fi`,
+      `Washing machine`,
+      `Towels`,
+      `Heating`,
+      `Coffee machine`,
+      `Baby seat`,
+      `Kitchen`,
+      `Dishwasher`,
+    ],
+    hostName: `Angelina`,
+    hostAvatar: `img/avatar-angelina.jpg`,
+    hostStatus: `pro`,
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.
+                  The building is green and from 18th century.`
+  }, {
     id: 400,
     isPremium: false,
     picture: `apartment-02.jpg`,
     price: 132,
     isBookmark: true,
     rating: 3.8,
-    name: `apartment-02`,
+    name: `Canal View Prinsengracht`,
     type: `Apartment`,
+
+    gallery: [
+      `apartment-01.jpg`,
+      `apartment-02.jpg`,
+      `apartment-01.jpg`,
+      `room.jpg`,
+      `apartment-03.jpg`,
+      `studio-01.jpg`,
+    ],
+    bedroomsAmount: 4,
+    maxAdults: 6,
+    features: [
+      `Wi-Fi`,
+      `Washing machine`,
+      `Towels`,
+      `Heating`,
+      `Coffee machine`,
+      `Dishwasher`,
+      `Cabel TV`,
+      `Fridge`
+    ],
+    hostName: `Max`,
+    hostAvatar: `img/avatar-max.jpg`,
+    hostStatus: `pro`,
+    description: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.
+                  An independent House, strategically located between Rembrand Square and National Opera,
+                  but where the bustle of the city comes to rest in this alley flowery and colorful.`
   }
 ];
 
