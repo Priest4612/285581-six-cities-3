48d0bb1316f39f43f0e2e72a103a2c28b1ff2b93
Priest4612 <prie4612@gmail.com>
2020-03-03 17:40:38 +0300

    Добавил props

--- src/components/review-item/review-item.jsx
+++ src/components/review-item/review-item.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 const ReviewItem = (props) => {
   const {review} = props;
+  const {/* id,  */rating, authorName, authorAvatar, description, dateTime} = review;
 
   if (!review) {
     return null;
@@ -12,30 +13,37 @@ const ReviewItem = (props) => {
     <li className="reviews__item">
       <div className="reviews__user user">
         <div className="reviews__avatar-wrapper user__avatar-wrapper">
-          <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar"/>
+          <img className="reviews__avatar user__avatar" src={`img/avatar-${authorAvatar}.jpg`} width="54" height="54" alt="Reviews avatar"/>
         </div>
         <span className="reviews__user-name">
-          Max
+          {authorName}
         </span>
       </div>
       <div className="reviews__info">
         <div className="reviews__rating rating">
           <div className="reviews__stars rating__stars">
-            <span style={{width: `80%`}}></span>
-            <span className="visually-hidden">Rating</span>
+            <span style={{width: (rating * 100 / 5) + `%`}}></span>
+            <span className="visually-hidden">{rating}</span>
           </div>
         </div>
         <p className="reviews__text">
-          A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
+          {description}
         </p>
-        <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
+        <time className="reviews__time" dateTime={dateTime}>{dateTime}</time>
       </div>
     </li>
   );
 };
 
 ReviewItem.propTypes = {
-  review: PropTypes.object.isRequired,
+  review: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    authorAvatar: PropTypes.string.isRequired,
+    authorName: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    dateTime: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default ReviewItem;
