b8278bde419495d6cf9296672b48f6ae7994e54f
priest4612 <prie4612@gmail.com>
2020-03-13 23:52:55 +0300

    Инициализировал хранилище

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
 import App from './components/app/app.jsx';
 import offers from './mock/offers.js';
+import {reducer} from './reducer.js';
 
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDom.render(
-    <App
-      offers={offers}
-    />,
+    <Provider store={store}>
+      <App
+        offers={offers}
+      />
+    </Provider>,
+
     document.querySelector(`#root`)
 );
